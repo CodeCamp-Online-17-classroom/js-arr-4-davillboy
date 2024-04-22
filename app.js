2729610a3b4c03efc584cbf10874dd23a99e95e3
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 22:30:36 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // write code here
+let numbers = []; // 
+
+while (true) {
+  let input = prompt("ป้อนตัวเลข:");
+  if (input === null || isNaN(input)) {
+    break; // ถ้าเป็น Cancel หรือไม่ใช่ตัวเลขให้หยุดวนลูป
+  }
+  
+  numbers.push(Number(input));
+}
+
+let sum = 0;
+for (let number of numbers) {
+  sum += number;
+}
+
+alert(`ผลรวมของตัวเลขทั้งหมดคือ: ${sum}`);
